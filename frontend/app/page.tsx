b864f79cf3200783e8c74d2b2eaee90a6117975c
Devansh kukreja <117558669+devansh1401@users.noreply.github.com>
2024-07-20 14:25:35 +0530

    prototype

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,12 +10,29 @@
 //   )
 // }
 
+// import GoogleSignInButton from "@/components/sign-in"
+
+// export default function Home() {
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <h1 className="text-4xl font-bold mb-8">Welcome to My Email Dashboard</h1>
+//       <GoogleSignInButton />
+//     </main>
+//   )
+// }
+
+
+
+
+
+
+
 import GoogleSignInButton from "@/components/sign-in"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to My Email Dashboard</h1>
+      <h1 className="text-4xl font-bold mb-8">Email Dashboard</h1>
       <GoogleSignInButton />
     </main>
   )
